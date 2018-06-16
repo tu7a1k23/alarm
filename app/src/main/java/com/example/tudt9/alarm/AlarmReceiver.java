9f9f97f480e25b9ee652a55d182277f88237889a
FSOFT.FPT.VN\TuDT9 <tudt9@fsoft.com.vn>
2018-06-16 21:28:14 +0700

    2

--- app/src/main/java/com/example/tudt9/alarm/AlarmReceiver.java
+++ app/src/main/java/com/example/tudt9/alarm/AlarmReceiver.java
@@ -3,7 +3,6 @@ package com.example.tudt9.alarm;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 
 /**
@@ -13,9 +12,7 @@ import android.util.Log;
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent){
-        Log.e("Toi Trong Receiver", "Xin Chao");
         String chuoi_string = intent.getExtras().getString("extra");
-        Log.e("Ban Truyen Key",chuoi_string);
 
         Intent myIntent = new Intent(context,Music.class);
         myIntent.putExtra("extra",chuoi_string);
